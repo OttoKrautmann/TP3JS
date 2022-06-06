2305f17e4e483995fcc97dff9cef978d285b1a48
Otto Krautmann <krautmann.otto@gmail.com>
2022-06-06 15:56:11 -0300

    termine el 1

---
+++ js/01.js
@@ -0,0 +1,6 @@
+let meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+let i = 0
+while (i < meses.length){
+    document.write(meses[i] + "<br>");
+    i++;
+}
